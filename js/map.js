cf2bfb53be49bf17838e5b3dd5ea671adf23b45b
VintPlanov <vintplanov@VintPlanov-PC.Home>
2020-01-16 18:15:09 +0500

    Создали массив с данными и открыли MAP

--- js/map.js
+++ js/map.js
@@ -1,9 +1,45 @@
 window.addEventListener("DOMContentLoaded", function(){
 	'use strict';
 
-	
+	var map = document.querySelector('.map');
 
+	var advertArr = [
+		{
+			"author": {
+				"avatar" : `img/avatars/user02.png`
+			}
+		},
+		{
+			"offer": {
+				"title": "Красивый гостевой домик",
+				"address": [ //"{{location.x}}, {{location.y}}"
+					600,
+					350
+				],
+				"price": 1000000,
+				"type": "flat",
+				"rooms": 5,
+				"guests": 8,
+				"checkin": '14:00',
+				"checkout": '12:00',
+				"features": ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"],
+				"description": '',
+				"photos": [
+					"http://o0.github.io/assets/images/tokyo/hotel1.jpg",
+					"http://o0.github.io/assets/images/tokyo/hotel2.jpg",
+					"http://o0.github.io/assets/images/tokyo/hotel3.jpg",
+					],
+			}
+		},
+		{
+			"location": {
+				x: 230,
+				y: 200
+			}
+		},
+	];
 
+	map.classList.remove('.map--faded');
 
 
 
