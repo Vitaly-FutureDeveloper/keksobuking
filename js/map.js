fc902dde48b931db67984a5bdd51f160d4b33434
Виталий <programmer@hithub.com>
2020-01-31 23:06:45 +0500

    Сделана валидация на запрет отправки формы, если гостей больше чем комнат...

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", function(){
-	'use strict';
+	//'use strict';
 
 	var map = document.querySelector('.map');
 	var mapPin = document.querySelector('.map__pin'),
@@ -41,7 +41,7 @@ window.addEventListener("DOMContentLoaded", function(){
 
 	};
 
-	map.classList.remove('map--faded');
+	//map.classList.remove('map--faded');
 
 	//Генератор случайных чисел для подстановки данных
 	function mt_rand(min, max){
@@ -136,6 +136,51 @@ window.addEventListener("DOMContentLoaded", function(){
 		mapPins.appendChild(fragment);
 	}
 
-createContentCards();
+	//createContentCards();
+
+//*******//
+//Events// >>
+//*******//
+
+	var buttonMapPinMain = document.querySelector('.map__pin--main');
+/*
+	var inputHousingType = document.querySelector('#housing-type'),
+		inputHousingPrice = document.querySelector('#housing-price'),
+		inputHousingRooms = document.querySelector('#housing-rooms'),
+		inputHousingGuests = document.querySelector('#housing-guests'),
+		inputHousingFeatures = document.querySelectorAll('input[name=features]');
+*/
+	var inputFormAddress = document.querySelector('#address'),
+		inputFormTitle = document.querySelector('#title'),
+		inputFormType = document.querySelector('#type'),
+		inputFormPrice = document.querySelector('#price'),
+		inputFormTimeIn = document.querySelector('#timein'),
+		inputFormTimeOut = document.querySelector('#timeout'),
+		inputFormRoom = document.querySelector('#room_number'),
+		inputFormGuests = document.querySelector('#capacity'),
+		inputFormFeatures = document.querySelectorAll('input[name=features]'),
+		inputFormDescription = document.querySelector('#description');
+
+	var noticeForm = document.querySelector('.notice__form');
+
+	buttonMapPinMain.addEventListener('mouseup', function(){ 
+		map.classList.remove('map--faded');
+
+		inputFormAddress.value = advertArr.offer.address;
+	});
+
+
+	noticeForm.addEventListener('submit', function(evt){
+
+		var inputGuestsValue = inputFormGuests.options[inputFormGuests.selectedIndex].value;
+		var inputRoomValue = inputFormRoom.options[inputFormRoom.selectedIndex].value;
+
+		if ( inputGuestsValue > inputRoomValue ){
+			evt.preventDefault();
+			document.querySelector('.errors').classList.remove('error--off');
+			document.querySelector('.error__item-guests').classList.remove('error--off');
+		}
+	});
+
 
 });
